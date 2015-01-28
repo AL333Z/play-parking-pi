a634d3b49e6b7013d077573b3213fb9e66f97720
al333z <alessandro.zoffoli@gmail.com>
2015-01-28 18:01:38 +0100

    Final touches

---
+++ app/devices/Config.java
@@ -0,0 +1,15 @@
+package devices;
+
+import com.pi4j.io.gpio.Pin;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class Config {
+	public static final Pin[] pinMap = 
+		{RaspiPin.GPIO_00,RaspiPin.GPIO_01,RaspiPin.GPIO_07,RaspiPin.GPIO_07,
+		RaspiPin.GPIO_07,RaspiPin.GPIO_07,RaspiPin.GPIO_07,RaspiPin.GPIO_07,
+		RaspiPin.GPIO_07,RaspiPin.GPIO_07,RaspiPin.GPIO_07,RaspiPin.GPIO_07,
+		RaspiPin.GPIO_07,RaspiPin.GPIO_07,RaspiPin.GPIO_07,RaspiPin.GPIO_07,
+		RaspiPin.GPIO_07,RaspiPin.GPIO_00,RaspiPin.GPIO_07,RaspiPin.GPIO_07,
+		};
+
+}
--- build.sbt
+++ build.sbt
@@ -7,7 +7,5 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq(
-  cache,
-  ws,
   "com.typesafe.akka" %% "akka-contrib" % "2.3.3"
 )
